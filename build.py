644f7ec6ec8f850676f0344f6f5c1dd19e7d2318
Sam <sam.mosleh@ut.ac.ir>
2020-03-12 13:32:20 +0330

    Fixed librdkafka typo.

--- build.py
+++ build.py
@@ -1,7 +1,9 @@
 from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
-    builder = ConanMultiPackager(visual_runtimes=["MD", "MDd"], build_policy="missing", upload_dependencies="all")
-    builder.add_common_builds(shared_option_name="librdkfaka:shared",
+    builder = ConanMultiPackager(visual_runtimes=["MD", "MDd"],
+                                 build_policy="missing",
+                                 upload_dependencies="all")
+    builder.add_common_builds(shared_option_name="librdkafka:shared",
                               pure_c=False)
     builder.run()
