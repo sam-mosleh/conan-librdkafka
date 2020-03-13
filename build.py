e35808c49ae9f1c4082ea7a6be6080936b22b8c0
Sam <sam.mosleh@ut.ac.ir>
2020-03-13 14:11:50 +0330

    Visual runtimes shouldnt be in add_common_builds.

--- build.py
+++ build.py
@@ -2,9 +2,9 @@ from cpt.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager(build_policy="missing",
-                                 upload_dependencies="all")
+                                 upload_dependencies="all",
+                                 visual_runtimes=["MD"])
     builder.add_common_builds(shared_option_name="librdkafka:shared",
                               pure_c=False,
-                              visual_runtimes=["MD"],
                               build_all_options_values=None)
     builder.run()
