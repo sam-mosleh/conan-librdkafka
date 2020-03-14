a97f77baba6ba83db721b8d43cbd4e5db8f60de3
Sam Mosleh <sam.mosleh@ut.ac.ir>
2020-03-14 20:16:48 +0330

    Removed unused variable from PackageTest.

--- test_package/test.cpp
+++ test_package/test.cpp
@@ -2,7 +2,6 @@
 #include <librdkafka/rdkafka.h>
 
 int main(int argc, char const *argv[]) {
-  rd_kafka_t *rk;
   rd_kafka_conf_t *conf = rd_kafka_conf_new();
   std::cout << std::endl
             << "----------------->Tests are done for now.<---------------------" << std::endl
